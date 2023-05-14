2af95ee0feaf4d7f516b0b33d1425dba2ae9cd54
sgmira <94264885+NeoMood@users.noreply.github.com>
2023-05-14 18:28:26 +0100

    Inception.dev

--- srcs/requirements/wordpress/conf/wordpress.sh
+++ srcs/requirements/wordpress/conf/wordpress.sh
@@ -11,31 +11,31 @@
 # NEW_USER_EMAIL="${NEW_USER_EMAIL:-neomood.2002@gmail.com}"
 # NEW_USER_PASSWORD="${NEW_USER_PASSWORD:-neomood02}"
 
-if [ -d "/var/www/html/wp-admin" ]; then
-  rm -rf /var/www/html/*
+if [ -d "/var/www/html/wordpress/wp-admin" ]; then
+  rm -rf /var/www/html/wordpress/*
 fi
 
-mkdir -p /var/www/html
+mkdir -p /var/www/html/wordpress
 # if ! wp core is-installed --allow-root; then
-    wp core download --path=/var/www/html --allow-root
-    wp config create --dbhost=mariadb --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --path=/var/www/html --dbhost=mariadb --allow-root
-    wp core install  --url=localhost --title="$SITE_TITLE" --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PASSWORD" --admin_email="$ADMIN_EMAIL" --path=/var/www/html --skip-email --allow-root
-    wp user create "$NEW_USER" "$NEW_USER_EMAIL" --role=author --user_pass="$NEW_USER_PASSWORD" --path=/var/www/html --allow-root
-    wp theme install astra --activate --path=/var/www/html --allow-root
-    wp theme activate astra --path=/var/www/html --allow-root
+    wp core download --path=/var/www/html/wordpress --allow-root
+    wp config create --dbhost=mariadb --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --path=/var/www/html/wordpress --dbhost=mariadb --allow-root
+    wp core install  --url=localhost --title="$SITE_TITLE" --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PASSWORD" --admin_email="$ADMIN_EMAIL" --path=/var/www/html/wordpress --skip-email --allow-root
+    wp user create "$NEW_USER" "$NEW_USER_EMAIL" --role=author --user_pass="$NEW_USER_PASSWORD" --path=/var/www/html/wordpress --allow-root
+    wp theme install astra --activate --path=/var/www/html/wordpress --allow-root
+    wp theme activate astra --path=/var/www/html/wordpress --allow-root
     # echo "\n\nchecking the theme\n\n"
-    # wp theme list --path=/var/www/html --allow-root
+    # wp theme list --path=/var/www/html/wordpress --allow-root
 # fi
 
 # Set up Redis Object Cache plugin
-wp plugin install redis-cache --activate --path=/var/www/html --allow-root
-wp redis enable --path=/var/www/html --allow-root
+wp plugin install redis-cache --activate --path=/var/www/html/wordpress --allow-root
+wp redis enable --path=/var/www/html/wordpress --allow-root
 
 # Configure Redis Object Cache
-wp config set WP_REDIS_HOST 'redis' --path=/var/www/html --allow-root
-wp config set WP_REDIS_PORT '6379' --path=/var/www/html --allow-root
-wp config set WP_REDIS_DATABASE '0' --path=/var/www/html --allow-root
-wp config set WP_CACHE_KEY_SALT 'your-unique-salt-here' --path=/var/www/html --allow-root
+wp config set WP_REDIS_HOST 'redis' --path=/var/www/html/wordpress --allow-root
+wp config set WP_REDIS_PORT '6379' --path=/var/www/html/wordpress --allow-root
+wp config set WP_REDIS_DATABASE '0' --path=/var/www/html/wordpress --allow-root
+wp config set WP_CACHE_KEY_SALT 'your-unique-salt-here' --path=/var/www/html/wordpress --allow-root
 
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
