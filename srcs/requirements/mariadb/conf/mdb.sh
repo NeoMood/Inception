c860819b35417f3a6136157ac6d029ddfe257738
Saad Gmira <sgmira@e2r7p14.1337.ma>
2023-05-15 17:55:35 +0100

    Inception

--- srcs/requirements/mariadb/conf/mdb.sh
+++ srcs/requirements/mariadb/conf/mdb.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# DB_NAME="${DB_NAME:-saad}"
-# DB_USER="${DB_USER:-NEO}"
-# DB_PASSWORD="${DB_PASSWORD:-neomood02}"
-
 service mysql start
 
 if [ ! -d "/var/lib/mysql/${DB_NAME}" ]; then
@@ -11,7 +7,6 @@ if [ ! -d "/var/lib/mysql/${DB_NAME}" ]; then
     mysql -u root -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}' ;"
     mysql -u root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' ;"
     mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ALTERUSER_PASS}' ;"
-    # mysql -u root -p12345 -e "FLUSH PRIVILEGES;"
 fi
 
 sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
